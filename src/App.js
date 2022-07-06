e7e675b26ae20525580a0000912cb2ab9fa4a019
Racyn Ibaya <racynibaya@outlook.com>
2022-07-06 04:30:38 -0400

    fixing error

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ import particleOptions from './components/Config/particlesConfig';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit(url) {
+    console.log('Click');
+
+    // app.models.predict('ddedab0c5dd94e1589aebfe97f4fadff', url);
+  }
+
   render() {
     return (
       <div className='App'>
@@ -16,7 +33,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
 
         {/*
         <FaceRecognition /> */}
