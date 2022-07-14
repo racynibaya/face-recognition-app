187f088d9236e672ea906e888b953ed966461836
Racyn Ibaya <racynibaya@outlook.com>
2022-07-14 05:42:11 -0400

    fixing the submition

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,11 +4,11 @@ const Register = ({ onRouteChange }) => {
   return (
     <article className='card br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
       <main className='pa4 black-80'>
-        <form className='measure'>
+        <div className='measure'>
           <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
             <legend className='f1 fw6 ph0 mh0'>Register</legend>
             <div className='mt3'>
-              <label className='db fw6 lh-copy f6' for='name'>
+              <label className='db fw6 lh-copy f6' htmlFor='name'>
                 Name
               </label>
               <input
@@ -20,7 +20,7 @@ const Register = ({ onRouteChange }) => {
             </div>
 
             <div className='mt3'>
-              <label className='db fw6 lh-copy f6' for='email-address'>
+              <label className='db fw6 lh-copy f6' htmlFor='email-address'>
                 Email
               </label>
               <input
@@ -32,7 +32,7 @@ const Register = ({ onRouteChange }) => {
             </div>
 
             <div className='mv3'>
-              <label className='db fw6 lh-copy f6' for='password'>
+              <label className='db fw6 lh-copy f6' htmlFor='password'>
                 Password
               </label>
               <input
@@ -52,7 +52,7 @@ const Register = ({ onRouteChange }) => {
               />
             </div>
           </fieldset>
-        </form>
+        </div>
       </main>
     </article>
   );
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -5,11 +5,11 @@ const Signin = ({ onRouteChange }) => {
     // This is a tachyons form
     <article className='card br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
       <main className='pa4 black-80'>
-        <form className='measure'>
+        <div className='measure'>
           <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
             <legend className='f1 fw6 ph0 mh0'>Sign In</legend>
             <div className='mt3'>
-              <label className='db fw6 lh-copy f6' for='email-address'>
+              <label className='db fw6 lh-copy f6' htmlFor='email-address'>
                 Email
               </label>
               <input
@@ -20,7 +20,7 @@ const Signin = ({ onRouteChange }) => {
               />
             </div>
             <div className='mv3'>
-              <label className='db fw6 lh-copy f6' for='password'>
+              <label className='db fw6 lh-copy f6' htmlFor='password'>
                 Password
               </label>
               <input
@@ -47,7 +47,7 @@ const Signin = ({ onRouteChange }) => {
               Register
             </p>
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );
